f9a0e67b42e99f9848dde65112ea5b11dbb4bec3
lexuanlian <loothy0104@gmail.com>
2024-12-15 17:26:36 +0000

    3

--- expressions/expressions.py
+++ expressions/expressions.py
@@ -1,3 +1,7 @@
+import numbers
+from functools import singledispatch
+
+
 class Expression():
 
     def __init__(self, *operands):
@@ -89,7 +93,7 @@ class Terminal(Expression):
 
 class Number(Terminal):
     def __init__(self, value):
-        if not isinstance(value, (int, float)):
+        if not isinstance(value, numbers.Number):
             raise TypeError("Number must be an int or float.")
         super().__init__(value)
 
@@ -119,3 +123,42 @@ def postvisitor(expr, fn, **kwargs):
             visited[e] = fn(e, *(visited[o] for o in e.operands), **kwargs)
 
     return visited[expr]
+
+
+@singledispatch
+def differentiate(expr, *o, var=None):
+    raise TypeError(f"Cannot differentiate expressions of type {type(expr)}")
+
+@differentiate.register(Number)
+def _(expr, *o, var=None):
+    return Number(0)
+
+@differentiate.register(Symbol)
+def _(expr, *o, var=None):
+    return Number(1 if expr.value == var else 0)
+
+@differentiate.register(Add)
+def _(expr, *o, var=None):
+    return Add(*o)
+
+@differentiate.register(Sub)
+def _(expr, *o, var=None):
+    return Sub(*o)
+
+@differentiate.register(Mul)
+def _(expr, *o, var=None):
+    u, v = expr.operands
+    du_dx, dv_dx = o
+    return Add(Mul(du_dx, v), Mul(u, dv_dx))
+
+@differentiate.register(Div)
+def _(expr, *o, var=None):
+    u, v = expr.operands
+    du_dx, dv_dx = o
+    return Div(Sub(Mul(du_dx, v), Mul(u, dv_dx)), Pow(v, Number(2)))
+
+@differentiate.register(Pow)
+def _(expr, *o, var=None):
+    u, v = expr.operands
+    du_dx, dv_dx = o
+    return Mul(Mul(v, Pow(u, Sub(v, Number(1)))), du_dx)
